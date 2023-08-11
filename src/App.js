63837a10ee44b6bb44d39cb9690b10f9fe56941c
aiden0407 <cailyent0407@gmail.com>
2023-08-11 14:36:28 +0900

    [Chore] App.js 기본 세팅

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
+//React
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Provider } from 'context';
+
+//Components
+import Layout from 'components/Layout';
+
+//Pages
+import Login from 'pages/Login';
+import Calendar from 'pages/Calendar';
+
 function App() {
   return (
-    <div>
-      Learn React
-    </div>
+    <Provider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+
+          <Route path="/*"
+            element={
+              <Layout>
+                <Routes>
+                  <Route path="/calendar" element={<Calendar />} />
+                </Routes>
+              </Layout>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
