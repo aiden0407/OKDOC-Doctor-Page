680d3174fa9e828b677ea8356e3cf2e981661642
aiden0407 <cailyent0407@gmail.com>
2023-08-11 14:39:12 +0900

    [Add] design 상수 파일 추가

---
+++ src/constants/design.js
@@ -0,0 +1,50 @@
+export const COLOR = {
+    MAIN: '#5500CC',
+    SUB1: '#3E0095',
+    SUB2: '#E3D0FF',
+    SUB3: '#F1E8FF',
+    SUB4: '#FAF6FF',
+    GRAY0: '#666666',
+    GRAY1: '#7A7A7A',
+    GRAY2: '#858585',
+    GRAY3: '#AAAAAA',
+    GRAY4: '#DBDBDB',
+    GRAY5: '#E8E8E8',
+    GRAY6: '#F2F2F5',
+    GRAY7: '#F7F8FA',
+}
+
+export const TYPOGRAPHY = {
+    T1: {
+        SIZE: '32px',
+        LEADING: '40px',
+    },
+    T2: {
+        SIZE: '24px',
+        LEADING: '32px',
+    },
+    T3: {
+        SIZE: '20px',
+        LEADING: '28px',
+    },
+    T4: {
+        SIZE: '18px',
+        LEADING: '26px',
+    },
+    T5: {
+        SIZE: '16px',
+        LEADING: '24px',
+    },
+    T6: {
+        SIZE: '14px',
+        LEADING: '20px',
+    },
+    T7: {
+        SIZE: '12px',
+        LEADING: '16px',
+    },
+    T8: {
+        SIZE: '11px',
+        LEADING: '16px',
+    },
+}
