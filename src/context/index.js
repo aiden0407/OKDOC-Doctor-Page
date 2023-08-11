e6831b4387acce754b3bdf8706c83b60a4025f48
aiden0407 <cailyent0407@gmail.com>
2023-08-11 14:38:03 +0900

    [Add] Context 파일 추가

---
+++ src/context/index.js
@@ -0,0 +1,40 @@
+//React
+import { createContext, useReducer } from "react";
+
+//initial state
+const initialState = {
+  isMenuTabOpened: false,
+};
+
+//create context
+const Context = createContext({});
+
+//create reducer
+const reducer = (state, action) => {
+  switch (action.type) {
+
+    case 'OPEN_MENU_TAB':
+      return {
+        ...state,
+        isMenuTabOpened: true,
+      };
+
+    case 'CLOSE_MENU_TAB':
+      return {
+        ...state,
+        isMenuTabOpened: false,
+      };
+
+    default:
+      return state;
+  }
+};
+
+//create Provider component
+const Provider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const value = { state, dispatch };
+  return <Context.Provider value={value}>{children}</Context.Provider>;
+};
+
+export { Context, Provider };
