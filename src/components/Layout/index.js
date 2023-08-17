240b503369c7eb64335ac121938606be36c6ed88
aiden0407 <cailyent0407@gmail.com>
2023-08-17 17:30:06 +0900

    [Design] 레이아웃 크스롤 적용 및 애니메이션 최적화

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -42,13 +42,15 @@ const BodyContainer = styled.div`
   width: ${(props) => props.isMenuTabOpened ? 'calc(100% - 250px)' : '100%'};
   margin-left: ${(props) => props.isMenuTabOpened ? '250px' : '0px'};
   height: calc(100vh - 70px);
-  transition: margin-left 0.3s;
-  padding: 40px 30px;
+  transition: all 0.3s;
+  padding: 0 30px;
   display: flex;
+  overflow-y: scroll;
   justify-content: center;
 `;
 
 const ContentsContainer = styled.div`
+  margin-top: 40px;
   width: 100%;
   max-width: 1280px;
   display: flex;
