e38be2fdbc0aef539d07f700af64a9e9bef88667
aiden0407 <cailyent0407@gmail.com>
2023-08-17 13:20:07 +0900

    [Update] 메뉴 탭 애니메이션 적용

--- src/components/MenuTab/index.js
+++ src/components/MenuTab/index.js
@@ -8,7 +8,7 @@ import styled from 'styled-components';
 import { COLOR } from 'constants/design';
 import { Text } from 'components/Text';
 import { Image } from 'components/Image';
-import { Row, FlexBox } from 'components/Flex';
+import { Row, Column, FlexBox } from 'components/Flex';
 
 //Assets
 import arrowIcon from 'assets/icons/arrow.svg';
@@ -19,11 +19,14 @@ function MenuTab() {
   const location = useLocation();
   const { state: { isMenuTabOpened } } = useContext(Context);
   const [menuStatus, setMenuStatus] = useState('');
+  const [isFirstMenuHovered, setIsFirstMenuHovered] = useState(false);
+  const [isSecondMenuHovered, setIsSecondMenuHovered] = useState(false);
 
   useEffect(() => {
     switch (location.pathname) {
       case '/calendar':
       case '/calendar/detail':
+      case '/telemedicine':
         setMenuStatus('원격진료 진료 현황');
         break;
 
@@ -47,17 +50,38 @@ function MenuTab() {
 
   return (
     <MenuTabContainer isMenuTabOpened={isMenuTabOpened}>
-      <StyledRow padding="0 10px">
-        <Text T5 bold>원격진료 관리</Text>
-        <FlexBox />
-        <Image src={arrowIcon} width={20} />
-      </StyledRow>
 
-      <StyledRow padding="0 10px">
-        <Text T5 bold>설정</Text>
-        <FlexBox />
-        <Image src={arrowIcon} width={20} />
-      </StyledRow>
+      <Column onMouseEnter={()=>setIsFirstMenuHovered(true)} onMouseLeave={()=>setIsFirstMenuHovered(false)}>
+        <StyledRow padding="0 10px" opened={menuStatus==="원격진료 진료 현황" || menuStatus==="진료 스케줄 관리" || isFirstMenuHovered}>
+          <Text T5 bold={menuStatus==="원격진료 진료 현황" || menuStatus==="진료 스케줄 관리" || isFirstMenuHovered}>원격진료 관리</Text>
+          <FlexBox />
+          <Image src={arrowIcon} width={20} className={(menuStatus==="원격진료 진료 현황" || menuStatus==="진료 스케줄 관리" || isFirstMenuHovered) && 'open'} />
+        </StyledRow>
+        <SubMenuWrapper className={(menuStatus==="원격진료 진료 현황" || menuStatus==="진료 스케줄 관리" || isFirstMenuHovered) && 'open'}>
+          <SubMenuButton className={menuStatus === "원격진료 진료 현황" && 'selected'} onClick={()=>navigate('/calendar')}>
+            <Text T5 bold={menuStatus === "원격진료 진료 현황"} color={menuStatus === "원격진료 진료 현황" && COLOR.MAIN}>원격진료 진료 현황</Text>
+          </SubMenuButton>
+          <SubMenuButton className={menuStatus === "진료 스케줄 관리" && 'selected'} onClick={()=>navigate('/schedule')}>
+            <Text T5 bold={menuStatus === "진료 스케줄 관리"} color={menuStatus === "진료 스케줄 관리" && COLOR.MAIN}>진료 스케줄 관리</Text>
+          </SubMenuButton>
+        </SubMenuWrapper>
+      </Column>
+
+      <Column onMouseEnter={()=>setIsSecondMenuHovered(true)} onMouseLeave={()=>setIsSecondMenuHovered(false)}>
+        <StyledRow padding="0 10px" marginTop={20} opened={menuStatus==="개인정보" || menuStatus==="비밀번호 변경" || isSecondMenuHovered}>
+          <Text T5 bold={menuStatus==="개인정보" || menuStatus==="비밀번호 변경" || isSecondMenuHovered}>설정</Text>
+          <FlexBox />
+          <Image src={arrowIcon} width={20} className={(menuStatus==="개인정보" || menuStatus==="비밀번호 변경" || isSecondMenuHovered) && 'open'} />
+        </StyledRow>
+        <SubMenuWrapper className={(menuStatus==="개인정보" || menuStatus==="비밀번호 변경" || isSecondMenuHovered) && 'open'}>
+          <SubMenuButton className={menuStatus === "개인정보" && 'selected'} onClick={()=>navigate('/setting/info')}>
+            <Text T5 bold={menuStatus === "개인정보"} color={menuStatus === "개인정보" && COLOR.MAIN}>개인정보</Text>
+          </SubMenuButton>
+          <SubMenuButton className={menuStatus === "비밀번호 변경" && 'selected'} onClick={()=>navigate('/setting/pw')}>
+            <Text T5 bold={menuStatus === "비밀번호 변경"} color={menuStatus === "비밀번호 변경" && COLOR.MAIN}>비밀번호 변경</Text>
+          </SubMenuButton>
+        </SubMenuWrapper>
+      </Column>
     </MenuTabContainer>
   )
 }
@@ -74,11 +98,36 @@ const MenuTabContainer = styled.div`
   border-right: 1px solid ${COLOR.GRAY4};
   display: flex;
   flex-direction: column;
-  gap: 20px;
   transition: all 0.3s;
 `;
 
 const StyledRow = styled(Row)`
   padding: ${(props) => props.padding ?? '0'};
   cursor: pointer;
+`;
+
+const SubMenuWrapper = styled.div`
+  margin-top: 10px;
+  max-height: 0;
+  overflow: hidden;
+  transition: all 0.3s ease-in-out;
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+  &.open {
+    max-height: 1000px;
+  }
+`;
+
+const SubMenuButton = styled.div`
+  width: 100%;
+  padding: 10px 0 10px 30px;
+  border-radius: 15px;
+  cursor: pointer;
+  &:hover {
+    background: #F1E8FF66;
+  }
+  &.selected {
+    background: ${COLOR.SUB3};
+  }
 `;
