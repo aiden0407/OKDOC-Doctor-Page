708b3c06bcc6ff58e5898dc797bc908995c4d20c
aiden0407 <cailyent0407@gmail.com>
2023-08-18 20:03:37 +0900

    [Update] 달력 라이브러리 적용 test

--- src/pages/Calendar/index.js
+++ src/pages/Calendar/index.js
@@ -10,15 +10,46 @@ import { Text } from 'components/Text';
 import { Image } from 'components/Image';
 import { Row, FlexBox, DividingLine } from 'components/Flex';
 
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
+
 function Calendar() {
 
   const navigate = useNavigate();
+  const [events, setEvents] = useState([
+    { title: 'event 1', date: '2023-08-01', url: '/calendar/detail' },
+    { title: 'event 2', date: '2023-08-02', url: '/calendar/detail' }
+  ]);
 
   return (
     <>
       <Text T2 bold>원격진료 진료 현황</Text>
-      <Row>
-      </Row>
+      <FullCalendar
+        plugins={[dayGridPlugin, interactionPlugin]}
+        initialView="dayGridMonth"
+        dateClick={(info) => {
+          console.log(info);
+          const eventNamePrompt = prompt("Enter, event name");
+          if (eventNamePrompt) {
+            setEvents([
+              ...events,
+              {
+                date: info.dateStr,
+                title: eventNamePrompt,
+                url: '/calendar/detail'
+              },
+            ]);
+          }
+        }}
+        events={events}
+        eventClick={function(info) {
+          info.jsEvent.preventDefault();
+          if (info.event.url) {
+            navigate(`${info.event.url}`);
+          }
+        }}
+      />
     </>
   );
 }
