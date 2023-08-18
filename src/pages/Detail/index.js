91191e87fd0192506da1d77486a06b20be15fb14
aiden0407 <cailyent0407@gmail.com>
2023-08-18 20:04:30 +0900

    [Update] 디테일 페이지 route에 따른 제목 변경

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -10,14 +10,36 @@ import { Text } from 'components/Text';
 import { Image } from 'components/Image';
 import { Row, FlexBox, DividingLine } from 'components/Flex';
 
+//Assets
+import arrowIcon from 'assets/icons/arrow_solid.svg';
+
 function Calendar() {
 
   const navigate = useNavigate();
+  const location = useLocation();
+  const [menuStatus, setMenuStatus] = useState('');
+
+  useEffect(() => {
+    switch (location.pathname) {
+      case '/calendar/detail':
+        setMenuStatus('원격진료 진료 현황');
+        break;
+
+      case '/schedule/detail':
+        setMenuStatus('진료 스케줄 관리');
+        break;
+
+      default:
+        break;
+    }
+  }, [location]);
 
   return (
     <>
-      <Text T2 bold>원격진료 진료 현황</Text>
       <Row>
+        <Text T2 bold color={COLOR.GRAY3}>{menuStatus}</Text>
+        <Image src={arrowIcon} width={32} marginLeft={10} />
+        <Text T2 bold color={COLOR.MAIN} marginLeft={10}>진료 상세</Text>
       </Row>
     </>
   );
