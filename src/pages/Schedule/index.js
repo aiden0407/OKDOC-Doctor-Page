e42c11b78390b9f274a1ad05b7639f6a9adfe271
aiden0407 <cailyent0407@gmail.com>
2023-08-18 20:03:58 +0900

    [Update] 달력 라이브러리 적용, 주간 그리드 test

--- src/pages/Schedule/index.js
+++ src/pages/Schedule/index.js
@@ -10,6 +10,10 @@ import { Text } from 'components/Text';
 import { Image } from 'components/Image';
 import { Row, FlexBox, DividingLine } from 'components/Flex';
 
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
+
 function Calendar() {
 
   const navigate = useNavigate();
@@ -17,8 +21,16 @@ function Calendar() {
   return (
     <>
       <Text T2 bold>진료 스케줄 관리</Text>
-      <Row>
-      </Row>
+      <FullCalendar
+        editable
+        selectable
+        plugins={[dayGridPlugin, interactionPlugin]}
+        initialView="dayGridWeek"
+        events={[
+          { title: 'event 1', date: '2023-08-18' },
+          { title: 'event 2', date: '2023-08-18' }
+        ]}
+      />
     </>
   );
 }
