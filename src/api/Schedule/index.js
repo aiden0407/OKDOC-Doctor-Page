7c4b09afb9d1ea42b61b6d89b98ef8b06533bd90
aiden0407 <cailyent0407@gmail.com>
2023-08-22 14:58:33 +0900

    [Update] 의사 스케줄 요청 api 변수 수정

--- src/api/Schedule/index.js
+++ src/api/Schedule/index.js
@@ -4,8 +4,7 @@ import axios from 'axios';
 export const getScheduleByDoctorId = async function (doctorId) {
     try {
         let options = {
-            //url: `${APIURL}/treatment_appointments/?doctor_id=${doctorId}`,
-            url: `${process.env.REACT_APP_API_HOST}/treatment_appointments/?doctor_id=b1fe2079-df9b-411a-93ee-bf21cb0c77bd`,
+            url: `${process.env.REACT_APP_API_HOST}/treatment_appointments/?doctor_id=${doctorId}`,
             method: 'GET',
         }
         const response = await axios(options);
