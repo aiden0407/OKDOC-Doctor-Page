ba4d158a3957fbf975a975ab803c0ee32a3a5e29
aiden0407 <cailyent0407@gmail.com>
2023-08-22 15:02:18 +0900

    [Update] 스케줄 오픈 event 생성 로직 추가

--- src/pages/Schedule/index.js
+++ src/pages/Schedule/index.js
@@ -5,31 +5,111 @@ import { useNavigate, useLocation } from 'react-router-dom';
 import styled from 'styled-components';
 
 //Components
-import { COLOR } from 'constants/design';
+import { COLOR } from 'design/constant';
 import { Text } from 'components/Text';
 import { Image } from 'components/Image';
 import { Row, FlexBox, DividingLine } from 'components/Flex';
 
+//FullCalendar
 import FullCalendar from '@fullcalendar/react';
-import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid'
 import interactionPlugin from '@fullcalendar/interaction';
+import 'design/fullcalendar.css';
+
+//Api
+import { getScheduleByDoctorId } from 'api/Schedule';
 
 function Calendar() {
 
   const navigate = useNavigate();
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    const sessionStorageData = sessionStorage.getItem('OKDOC_DOCTOR');
+    if(sessionStorageData){
+      const storedLoginData = JSON.parse(sessionStorageData);
+      initScheduleData(storedLoginData)
+    }
+  }, []);
+
+  const initScheduleData = async function (loginData) {
+    try {
+      const response = await getScheduleByDoctorId(loginData.id);
+      let scheduleList = [];
+
+      response.data.response.forEach((appointment) => {
+        scheduleList.push({
+          title: `• ${appointment.patient.passport.user_name} / ${formatTimeFromISOString(appointment.wish_at)} / ${appointment.status === 'RESERVATION_CONFIRMED' ? '예약' : '완료'}`,
+          start: appointment.wish_at,
+          end: addMinutesToDate(appointment.wish_at),
+          url: `/calendar/detail?id=${appointment.id}`,
+        });
+      });
+
+      scheduleList.push({
+        title: `• 이준범 / ${formatTimeFromISOString("2023-08-22T16:00:00.000Z")} / 예약`,
+        start: "2023-08-22T16:00:00.000Z",
+        end: addMinutesToDate("2023-08-22T16:00:00.000Z"),
+        url: `/calendar/detail?id=${1234}`,
+      });
+
+      setEvents(scheduleList);
+
+    } catch (error) {
+      alert('네트워크 오류로 인해 정보를 불러오지 못했습니다.');
+    }
+  }
+
+  function formatTimeFromISOString(dateString) {
+    const date = new Date(dateString);
+    console.log(date);
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const formattedTime = `${hours}:${minutes}`;
+    return formattedTime;
+  }
+
+  function addMinutesToDate(dateString) {
+    const inputDate = new Date(dateString);
+    const newDate = new Date(inputDate.getTime() + 20 * 60 * 1000);
+    return newDate.toISOString();
+  }
+
+  const handleSelect = (info) => {
+    const { start, end } = info;
+    setEvents([
+      ...events,
+      {
+        start,
+        end
+      },
+    ]);
+
+  };
 
   return (
     <>
       <Text T2 bold>진료 스케줄 관리</Text>
       <FullCalendar
-        editable
         selectable
-        plugins={[dayGridPlugin, interactionPlugin]}
-        initialView="dayGridWeek"
-        events={[
-          { title: 'event 1', date: '2023-08-18' },
-          { title: 'event 2', date: '2023-08-18' }
-        ]}
+        plugins={[timeGridPlugin, interactionPlugin]}
+        initialView="timeGridWeek"
+        contentHeight={650}
+        headerToolbar={{
+          start: 'title',
+          center: '',
+          end: 'prev,next'
+        }}
+        events={events}
+
+        select={handleSelect}
+
+
+
+
+        eventColor={COLOR.SUB3}
+        nowIndicator={true}
+        locale="kr"
       />
     </>
   );
