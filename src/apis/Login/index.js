8f3e18b98b0d2a7c3669009a526bd09a7c3e1376
aiden0407 <cailyent0407@gmail.com>
2023-08-24 17:00:58 +0900

    [Update] 의사 토큰으로 의사 정보 조회

--- src/apis/Login/index.js
+++ src/apis/Login/index.js
@@ -1,14 +1,31 @@
 //API
 import axios from 'axios';
 
-export const doctorLogin = async function (id, password) {
+export const doctorLogin = async function (email, password) {
     try {
         let options = {
-            url: `${process.env.REACT_APP_API_HOST}/authentication/sign-in`,
+            url: `${process.env.REACT_APP_API_HOST}/authentication/doctor/sign-in`,
             method: 'POST',
             data: {
-                id: id,
+                id: email,
                 password: password,
+            }
+        }
+        const response = await axios(options);
+        return response;
+
+    } catch (error) {
+        throw error;
+    }
+}
+
+export const getDoctorInfoByCredential = async function (accessToken) {
+    try {
+        let options = {
+            url: `${process.env.REACT_APP_API_HOST}/doctors`,
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${accessToken}`,
             }
         }
         const response = await axios(options);
