2edababae1dfca084fb6671de6f69cdd2eadc757
aiden0407 <cailyent0407@gmail.com>
2023-08-25 16:34:35 +0900

    [Update] 비밀번호 변경 api 추가

--- src/apis/Setting/index.js
+++ src/apis/Setting/index.js
@@ -32,3 +32,25 @@ export const editDoctorInfo = async function (accessToken, doctorId, data) {
         throw error;
     }
 }
+
+export const changePassword = async function (accessToken, email, password, newPassword) {
+    try {
+        let options = {
+            url: `${process.env.REACT_APP_API_HOST}/authentication/doctor/change-password`,
+            method: 'POST',
+            headers: {
+                'Authorization': `Bearer ${accessToken}`,
+            },
+            data: {
+                email: email,
+                password: password,
+                new_password: newPassword
+            }
+        }
+        const response = await axios(options);
+        return response;
+
+    } catch (error) {
+        throw error;
+    }
+}
