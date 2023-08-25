122c407a69e916dea02f93f0f0bfceaab5166ead
aiden0407 <cailyent0407@gmail.com>
2023-08-25 16:05:12 +0900

    [Update] 의사 정보 수정 api 추가

--- src/apis/Setting/index.js
+++ src/apis/Setting/index.js
@@ -14,3 +14,21 @@ export const getDepartments = async function () {
         throw error.response;
     }
 }
+
+export const editDoctorInfo = async function (accessToken, doctorId, data) {
+    try {
+        let options = {
+            url: `${process.env.REACT_APP_API_HOST}/doctors/${doctorId}`,
+            method: 'PATCH',
+            headers: {
+                'Authorization': `Bearer ${accessToken}`,
+            },
+            data: data
+        }
+        const response = await axios(options);
+        return response;
+
+    } catch (error) {
+        throw error;
+    }
+}
