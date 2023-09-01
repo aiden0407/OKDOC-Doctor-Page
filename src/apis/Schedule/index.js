65284de7ddbd60a118928177d72a96740baed178
aiden0407 <cailyent0407@gmail.com>
2023-09-01 15:40:37 +0900

    [Update] 비딩 정보 조회 api 추가

--- src/apis/Schedule/index.js
+++ src/apis/Schedule/index.js
@@ -14,3 +14,20 @@ export const getScheduleByDoctorId = async function (doctorId) {
         throw error.response;
     }
 }
+
+export const getBiddingInformation = async function (loginToken, biddingId) {
+    try {
+        let options = {
+            url: `${process.env.REACT_APP_API_HOST}/biddings/${biddingId}`,
+            method: 'GET',
+            headers: {
+                Authorization: `Bearer ${loginToken}`
+            }
+        }
+        const response = await axios(options);
+        return response;
+
+    } catch (error) {
+        throw error.response;
+    }
+}
