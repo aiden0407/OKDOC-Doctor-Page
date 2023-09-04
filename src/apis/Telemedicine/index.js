1bfe28d6b282e06729a80d200ca4a1d3649445b6
aiden0407 <cailyent0407@gmail.com>
2023-09-05 08:45:37 +0900

    [Update] 환자 진료 기록 업데이트 api 추가

--- src/apis/Telemedicine/index.js
+++ src/apis/Telemedicine/index.js
@@ -1,25 +1,14 @@
 //API
 import axios from 'axios';
 
-export const getAppointmentInfoById = async function (appointmentId) {
+export const getPatientInfoById = async function (loginToken, patientId) {
     try {
         let options = {
-            url: `${process.env.REACT_APP_API_HOST}/treatment_appointments/${appointmentId}`,
+            url: `${process.env.REACT_APP_API_HOST}/patients/${patientId}`,
             method: 'GET',
-        }
-        const response = await axios(options);
-        return response;
-
-    } catch (error) {
-        throw error.response;
-    }
-}
-
-export const getAppointmentListByPatientId = async function (patientId) {
-    try {
-        let options = {
-            url: `${process.env.REACT_APP_API_HOST}/treatment_appointments/?patient_id=${patientId}`,
-            method: 'GET',
+            headers: {
+                Authorization: `Bearer ${loginToken}`
+            }
         }
         const response = await axios(options);
         return response;
@@ -34,6 +23,37 @@ export const getHistoryListByPatientId = async function (patientId) {
         let options = {
             url: `${process.env.REACT_APP_API_HOST}/purchase_histories/?patient_id=${patientId}`,
             method: 'GET',
+        }
+        const response = await axios(options);
+        return response;
+
+    } catch (error) {
+        throw error.response;
+    }
+}
+
+export const getHistoryStatus = async function (documentKey) {
+    try {
+        let options = {
+            url: `${process.env.REACT_APP_API_HOST}/purchase_histories/?documentKey=${documentKey}`,
+            method: 'GET',
+        }
+        const response = await axios(options);
+        return response;
+
+    } catch (error) {
+        throw error.response;
+    }
+}
+
+export const getTreatmentResults = async function (loginToken, treatmentId) {
+    try {
+        let options = {
+            url: `${process.env.REACT_APP_API_HOST}/treatments/?treatment_appointment_id=${treatmentId}`,
+            method: 'GET',
+            headers: {
+                Authorization: `Bearer ${loginToken}`
+            }
         }
         const response = await axios(options);
         return response;
