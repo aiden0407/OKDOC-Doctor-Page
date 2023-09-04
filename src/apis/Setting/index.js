67f49e52f95aa710352cd9041226a5f83f07347d
aiden0407 <cailyent0407@gmail.com>
2023-09-05 08:44:51 +0900

    [Temp] 의사 프로필 업데이트 api 수정

--- src/apis/Setting/index.js
+++ src/apis/Setting/index.js
@@ -1,5 +1,6 @@
 //API
 import axios from 'axios';
+import { v4 as uuidv4 } from 'uuid';
 
 export const getDepartments = async function () {
     try {
@@ -16,14 +17,32 @@ export const getDepartments = async function () {
 }
 
 export const editDoctorInfo = async function (accessToken, doctorId, data) {
+    const formData = new FormData();
+    formData.append('department', data.department);
+    data.strength.forEach(function(strength) {
+        formData.append('strength', strength);
+    });
+    data.field.forEach(function(field) {
+        formData.append('field', field);
+    });
+    formData.append('self_introduction_title', data.self_introduction_title);
+    formData.append('self_introduction', data.self_introduction);
+    // formData.append('photo', 'https://d2u3dcdbebyaiu.cloudfront.net/uploads/atch_img/309/59932b0eb046f9fa3e063b8875032edd_crop.jpeg');
+    // formData.append('images', {
+    //     uri: data.images,
+    //     type: 'image/jpeg',
+    //     name: `${uuidv4()}.jpg`,
+    // });
+
     try {
         let options = {
             url: `${process.env.REACT_APP_API_HOST}/doctors/${doctorId}`,
             method: 'PATCH',
             headers: {
                 'Authorization': `Bearer ${accessToken}`,
+                'Content-Type': 'multipart/form-data'
             },
-            data: data
+            data: formData,
         }
         const response = await axios(options);
         return response;
