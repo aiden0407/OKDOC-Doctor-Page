7530877cad04ab8259d2afcb6c68790c0d6a2d2f
aiden0407 <cailyent0407@gmail.com>
2023-09-06 09:07:42 +0900

    [Update] 의사 소견서 PDF화 로직 추가

--- src/pages/Telemedicine/index.js
+++ src/pages/Telemedicine/index.js
@@ -6,10 +6,10 @@ import moment from 'moment';
 import { saveAs } from 'file-saver';
 
 //Components
-import { COLOR, TYPOGRAPHY } from 'design/constant';
+import { COLOR } from 'design/constant';
 import { Text } from 'components/Text';
 import { Image } from 'components/Image';
-import { Row, Column, DividingLine } from 'components/Flex';
+import { Row, Column } from 'components/Flex';
 
 //Api
 import { getTreatmentInformation, getTreatmentByPatientId, getTreatmentResults, submitTreatment, findDeases } from 'apis/Telemedicine';
@@ -17,6 +17,9 @@ import { getBiddingInformation } from 'apis/Schedule';
 
 //Assets
 import folderIcon from 'assets/icons/folder.svg';
+
+//react-pdf
+import { Page, Text as PdfText, View, Font, Document, StyleSheet, PDFViewer } from '@react-pdf/renderer';
 
 function Telemedicine() {
 
@@ -27,6 +30,7 @@ function Telemedicine() {
 
   const [treatmentData, setTreatmentData] = useState();
   const [consultingData, setConsultingData] = useState([]);
+  const [isDiagnosisOpend, setIsDiagnosisOpend] = useState(false);
 
   const [CC, setCC] = useState('');
   const [subjectiveSymtoms, setSubjectiveSymtoms] = useState('');
@@ -129,7 +133,7 @@ function Telemedicine() {
 
   const handleTreatmentSubmit = async function () {
     if (!CC.length || !subjectiveSymtoms.length || !subjectiveSymtoms.length || !objectiveFindings.length || !diagnosisCode.length || !diagnosisType.length || !assessment.length || !plan.length || !medicalOpinion.length) {
-      alert('작성하지 않은 필드가 존재합니다.');
+      alert('MD 노트에 작성하지 않은 필드가 존재합니다.');
       return;
     }
 
@@ -156,11 +160,245 @@ function Telemedicine() {
     }
   }
 
-  console.log(treatmentData);
-  console.log(consultingData);
+  function returnToday() {
+    const currentDate = new Date();
+    const year = currentDate.getFullYear();
+    const month = String(currentDate.getMonth() + 1).padStart(2, '0'); // 월은 0부터 시작하므로 +1 해줍니다.
+    const day = String(currentDate.getDate()).padStart(2, '0');
+    return `${year}년 ${month}월 ${day}일`;
+  }
+
+  const MyDocument = () => (
+    <Document>
+      <Page size="A4" style={styles.page}>
+        <PdfText style={styles.main}>소견서</PdfText>
+
+        <View style={styles.row1}>
+          <View style={styles.titleBox1}>
+            <PdfText style={styles.title}>환자의 성명</PdfText>
+          </View>
+          <View style={styles.contentBox1}>
+            <PdfText style={styles.content}>{treatmentData?.patient?.passport?.user_name}</PdfText>
+          </View>
+          <View style={styles.titleBox1}>
+            <PdfText style={styles.title}>환자 생년월일</PdfText>
+          </View>
+          <View style={styles.contentBox1}>
+            <PdfText style={styles.content}>{formatDate(String(treatmentData?.patient?.passport?.birth))}</PdfText>
+          </View>
+        </View>
+
+        <View style={styles.row2}>
+          <View style={styles.titleBox2}>
+            <PdfText style={styles.title}>병명</PdfText>
+            <View style={styles.column1}>
+              <PdfText style={styles.content}>{diagnosisType === 'presumptive' ? '◉ 임상적 추정' : '○ 임상적 추정'}</PdfText>
+              <PdfText style={styles.content}>{diagnosisType === 'definitive' ? '◉ 최종 진단' : '○ 최종 진단'}</PdfText>
+            </View>
+          </View>
+          <View style={styles.contentBox2}>
+            <PdfText style={styles.content}>{diagnosis}</PdfText>
+          </View>
+          <View style={styles.titleBox2}>
+            <PdfText style={styles.title}>질병 분류 기호</PdfText>
+          </View>
+          <View style={styles.contentBox2}>
+            <PdfText style={styles.content}>{diagnosisCode}</PdfText>
+          </View>
+        </View>
+
+        <View style={styles.row2}>
+          <View style={styles.titleBox3}>
+            <PdfText style={styles.title}>진료 내용 및{'\n'}향후 치료에 대한{'\n'}소견</PdfText>
+          </View>
+          <View style={styles.contentBox3}>
+            <View style={styles.column2}>
+              <PdfText style={styles.title}>(환자 호소 증상)</PdfText>
+              <PdfText style={styles.content}>{CC}</PdfText>
+              <PdfText style={styles.content}>{subjectiveSymtoms}</PdfText>
+            </View>
+            
+            <View style={styles.column2}>
+              <PdfText style={styles.title}>(본 의사의 판단)</PdfText>
+              <PdfText style={styles.content}>{objectiveFindings}</PdfText>
+              <PdfText style={styles.content}>{assessment}</PdfText>
+            </View>
+            
+            <View style={styles.column2}>
+              <PdfText style={styles.title}>(치료 계획)</PdfText>
+              <PdfText style={styles.content}>{plan}</PdfText>
+            </View>
+            
+            <View style={styles.column2}>
+              <PdfText style={styles.title}>(본 의사의 판단)</PdfText>
+              <PdfText style={styles.content}>{medicalOpinion}</PdfText>
+            </View>
+          </View>
+        </View>
+
+        <View style={styles.contentBox4}>
+          <PdfText style={styles.content}>상기 진료는 ㈜인성정보의 OK DOC 플랫폼을 통한 원격진료로 진행되었으며, 위와 같이 소견합니다.</PdfText>
+          <View style={styles.row3}>
+            <PdfText style={styles.content}>{returnToday()}</PdfText>
+          </View>
+          <View style={styles.row4}>
+            <PdfText style={styles.content}>의료기관 명칭 : 의정부을지대학교병원</PdfText>
+            <PdfText style={styles.content}> </PdfText>
+            <PdfText style={styles.content}>주소 : 경기도 의정부시 동일로 712 (금오동)</PdfText>
+            <PdfText style={styles.content}> </PdfText>
+            <PdfText style={styles.content}> </PdfText>
+            <PdfText style={styles.content}> </PdfText>
+            <PdfText style={styles.content}>[○]의사 [ ]치과의사 [ ]한의사 면허번호 : 제 {treatmentData?.doctor?.id} 호</PdfText>
+            {/* <PdfText style={styles.content}>[○]의사 [&nbsp;&nbsp;]치과의사 [&nbsp;&nbsp;]한의사 면허번호 : 제 82595 호</PdfText> */}
+            <PdfText style={styles.content}> </PdfText>
+            <PdfText style={styles.content}>성명:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{treatmentData?.doctor?.name}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;(서명)</PdfText>
+            {/* <PdfText style={styles.content}>성명:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;임수빈&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;(서명)</PdfText> */}
+          </View>
+        </View>
+
+        
+      </Page>
+    </Document>
+  );
+
+  Font.register({ family: "Nanum Gothic Regular", src: "https://fonts.gstatic.com/ea/nanumgothic/v5/NanumGothic-Regular.ttf", });
+  Font.register({ family: "Nanum Gothic Bold", src: "https://fonts.gstatic.com/ea/nanumgothic/v5/NanumGothic-Bold.ttf", });
+
+  const styles = StyleSheet.create({
+    viewer: {
+      position: 'absolute',
+      width: '40%',
+      height: '80%',
+      top: 0,
+      right: 0
+    },
+    page: {
+      width: '100%',
+      height: '100%',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      padding: '25px 40px',
+      fontFamily: 'Nanum Gothic Regular'
+    },
+    main: {
+      fontFamily: 'Nanum Gothic Bold',
+      fontSize: 20
+    },
+    title: {
+      fontFamily: 'Nanum Gothic Bold',
+      fontSize: 11,
+      textAlign: 'center'
+    },
+    content: {
+      fontSize: 11
+    },
+    row1: {
+      marginTop: 30,
+      width: '100%',
+      display: 'flex',
+      flexDirection: 'row',
+    },
+    titleBox1: {
+      minWidth: 80,
+      height: 24,
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      border: '1px solid #7A7A7A',
+      backgroundColor: '#F0F0F0',
+    },
+    contentBox1: {
+      width: '100%',
+      height: 24,
+      padding: '0 0 0 10px',
+      display: 'flex',
+      justifyContent: 'center',
+      border: '1px solid #7A7A7A',
+    },
+    row2: {
+      width: '100%',
+      display: 'flex',
+      flexDirection: 'row',
+    },
+    column1: {
+      marginTop: 4,
+      marginLeft: 8,
+      width: '100%',
+      display: 'flex',
+      alignItems: 'flex-start',
+      gap: 4
+    },
+    titleBox2: {
+      minWidth: 80,
+      height: 75,
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      border: '1px solid #7A7A7A',
+      backgroundColor: '#F0F0F0',
+    },
+    contentBox2: {
+      width: '100%',
+      height: 75,
+      padding: '0 0 0 10px',
+      display: 'flex',
+      justifyContent: 'center',
+      border: '1px solid #7A7A7A',
+    },
+    titleBox3: {
+      minWidth: 80,
+      height: 460,
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      border: '1px solid #7A7A7A',
+      backgroundColor: '#F0F0F0',
+    },
+    contentBox3: {
+      width: '100%',
+      height: 460,
+      padding: '10px 0 60px 10px',
+      display: 'flex',
+      border: '1px solid #7A7A7A',
+      justifyContent: 'space-between',
+    },
+    column2: {
+      width: '100%',
+      display: 'flex',
+      alignItems: 'flex-start',
+      gap: 4
+    },
+    contentBox4: {
+      width: '100%',
+      height: 170,
+      padding: '10px 30px 30px 10px',
+      display: 'flex',
+      border: '1px solid #7A7A7A',
+    },
+    row3: {
+      marginTop: 10,
+      width: '100%',
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'flex-end',
+    },
+    row4: {
+      padding: '30px 0 0 30px',
+      width: '100%',
+      display: 'flex',
+      alignItems: 'center',
+    },
+  });
 
   return (
     <TelemedicineContainer>
+      {
+        isDiagnosisOpend && <PDFViewer style={styles.viewer}>
+          <MyDocument />
+        </PDFViewer>
+      }
+
       <TelemedicineSector1>
         <TelemedicineTitleBox>
           <Text T6 bold color="#565965">진료 예약 일자</Text>
@@ -173,6 +411,7 @@ function Telemedicine() {
         </TelemedicineTitleBox>
 
         <Iframe src={`https://zoom.okdoc.app/meeting/doctor/?meetingNumber=${treatmentData?.hospital_treatment_room?.id}&userName=${treatmentData?.doctor?.name} 의사&wishAt=${treatmentData?.biddingData?.wish_at}`} allow="camera; microphone" />
+        {/* <Iframe src={`http://127.0.0.1:5500/meeting/doctor/?meetingNumber=${treatmentData?.hospital_treatment_room?.id}&userName=${treatmentData?.doctor?.name} 의사&wishAt=${treatmentData?.biddingData?.wish_at}`} allow="camera; microphone" /> */}
       </TelemedicineSector1>
 
       <TelemedicineSector2>
@@ -434,8 +673,11 @@ function Telemedicine() {
           <LineButton onClick={()=>window.ChannelIO('showMessenger')}>
             <Text T6 medium>고객센터 연결</Text>
           </LineButton>
-          <LineButton onClick={()=>{}}>
-            <Text T6 medium>소견서 미리보기</Text>
+          <LineButton onClick={()=>window.open('https://forms.gle/RNpf1d3tvndL2Tfd7', '_blank')}>
+            <Text T6 medium>진료영상 첨부</Text>
+          </LineButton>
+          <LineButton onClick={()=>setIsDiagnosisOpend(!isDiagnosisOpend)}>
+            <Text T6 medium>{isDiagnosisOpend?'미리보기 닫기':'소견서 미리보기'}</Text>
           </LineButton>
           <SaveButton onClick={()=>handleTreatmentSubmit()}>
             <Text T6 medium color='#FFFFFF'>제출</Text>
@@ -449,6 +691,7 @@ function Telemedicine() {
 export default Telemedicine;
 
 const TelemedicineContainer = styled.div`
+  position: relative;
   width: 100%;
   height: 100%;
   padding-bottom: 30px;
