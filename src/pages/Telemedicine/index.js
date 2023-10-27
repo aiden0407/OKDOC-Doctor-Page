50fd103dbdcb5a7346fd3b79fd428dcc8eca5c84
aiden0407 <cailyent0407@gmail.com>
2023-10-27 13:12:39 +0900

    [Fix] 의사 진료실 leave 시간 변경

--- src/pages/Telemedicine/index.js
+++ src/pages/Telemedicine/index.js
@@ -608,7 +608,7 @@ function Telemedicine() {
           <Text T6 color="#565965">{moment(treatmentData?.hospital_treatment_room?.start_time).format('HH:mm')} ~ {moment(treatmentData?.hospital_treatment_room?.start_time).add(15, 'minutes').format('HH:mm')}</Text>
         </TelemedicineTitleBox>
 
-        <Iframe src={moment().isBefore(moment(treatmentData?.hospital_treatment_room?.start_time).add(5, 'minutes')) ? `https://zoom.okdoc.app/meeting/doctor/?meetingNumber=${treatmentData?.hospital_treatment_room?.id}&userName=${treatmentData?.doctor?.name} 의사&wishAt=${treatmentData?.biddingData?.wish_at}` : 'https://zoom.okdoc.app/meeting/doctor/end/'} sandbox="allow-same-origin allow-scripts allow-modals" allow="camera; microphone" />
+        <Iframe src={moment().isBefore(moment(treatmentData?.hospital_treatment_room?.start_time).add(15, 'minutes')) ? `https://zoom.okdoc.app/meeting/doctor/?meetingNumber=${treatmentData?.hospital_treatment_room?.id}&userName=${treatmentData?.doctor?.name} 의사&wishAt=${treatmentData?.biddingData?.wish_at}` : 'https://zoom.okdoc.app/meeting/doctor/end/'} sandbox="allow-same-origin allow-scripts allow-modals" allow="camera; microphone" />
         {/* <Iframe src={`https://zoom.okdoc.app/meeting/doctor/?meetingNumber=${treatmentData?.hospital_treatment_room?.id}&userName=${treatmentData?.doctor?.name} 의사&wishAt=${treatmentData?.biddingData?.wish_at}`} sandbox="allow-same-origin allow-scripts allow-modals" allow="camera; microphone" /> */}
         {/* <Iframe src={`http://127.0.0.1:5500/meeting/doctor/?meetingNumber=${treatmentData?.hospital_treatment_room?.id}&userName=${treatmentData?.doctor?.name} 의사&wishAt=2024-07-10T02:00:00.000Z`} allow="camera; microphone" /> */}
       </TelemedicineSector1>
